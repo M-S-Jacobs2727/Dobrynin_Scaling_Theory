e656cd58f298dba2569fea3f7d76623bde8819d2
Michael Shea Jacobs <M.S.Jacobs2727@gmail.com>
2022-04-28 17:14:21 -0400

    wrapping up test of train_network.py

--- Molecular-Fingerprint-Code/train_network.py
+++ Molecular-Fingerprint-Code/train_network.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 import json
 import torch
+from torch.utils.data import Dataset, DataLoader
 
 class NeuralNet(torch.nn.Module):
     def __init__(self, *num_per_layer):
@@ -22,31 +23,37 @@ class NeuralNet(torch.nn.Module):
         logits = self.linear_relu_stack(x)
         return logits
 
-class CustomDataLoader(torch.utils.data.Dataset):
+class CustomDataset(Dataset):
+    """Pixelated viscosity plots dataset. Each datum is a 32x32 grid of values
+    0 <= x <= 1. 
+    """
     def __init__(self, path):
+        """Args: 
+                path (string): Path to dataset
+        """
         self.path = path
-        self.filenames = os.list(self.path)
+        self.filenames = os.listdir(self.path)
     
     def __len__(self):
         return len(self.filenames)
     
     def __getitem__(self, idx):
         filename = self.filenames[idx]
-        data = np.loadtxt(os.path.join(self.path, filename)).flatten()
+        data = np.loadtxt(os.path.join(self.path, filename)).astype(np.float32)
 
         m = re.match(r'Bg_([0-9.]+)_Bth_([0-9.]+)_Pe_([0-9.]+)\.txt', filename)
         if not m:
             raise ValueError(f'Could not read parameter values from file'
                     ' {os.path.join(self.path, filename)}')
         params = np.array([float(i) for i in m.groups()])
-        params = self.normalize(*params)
+        params = self.normalize(*params).astype(np.float32)
 
         return data, params
 
     def normalize(self, Bg, Bth, Pe):
         Bg /= 2
         Pe /= 30
-        return Bg, Bth, Pe
+        return np.array([Bg, Bth, Pe])
 
 def train(dataloader, model, loss_fn, optimizer, device):
     size = len(dataloader.dataset)
@@ -69,48 +76,47 @@ def test(dataloader, model, loss_fn, device):
     size = len(dataloader.dataset)
     num_batches = len(dataloader)
     model.eval()
-    avg_loss, accuracy = 0, 0
+    avg_loss, avg_error = 0, 0
     with torch.no_grad():
         for X, y in dataloader:
             X, y = X.to(device), y.to(device)
             pred = model(X)
             avg_loss += loss_fn(pred, y).item()
-            accuracy += 1 - np.abs(pred - y) / y
+            avg_error += abs(pred - y) / y
         
     avg_loss /= num_batches
-    accuracy /= num_batches
-    print(f'Test Error:\n  {accuracy = :>0.3f}, {avg_loss = :>8f} \n')
-
-def get_num_start():
-    with open('Molecular-Fingerprint-Code/surface_bins.json') as f:
-        bin_data = json.load(f)
-    return bin_data['Nw_num_bins'] * bin_data['phi_num_bins']
+    avg_error /= num_batches
+    print(f'Test Error:  {avg_error = :.3f}, {avg_loss = :>8f}')
 
 def main():
-    num_first_layer = get_num_start()
+    random.seed(5)
     batch_size = 16
     train_size = 256
     test_size = 64
-    num_per_layer = [num_first_layer, 64, 64]
-    device = 'gpu' if torch.cuda.is_available() else "cpu"
+    
+    my_dataset = CustomDataset('../grid_data')
+    first_layer_shape = my_dataset[0][0].shape
+    first_layer_len = first_layer_shape[0] * first_layer_shape[1]
+    num_per_layer = [first_layer_len, 64, 64]
+
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
     model = NeuralNet(*num_per_layer).to(device)
-    print(model)
 
-    my_dataloader = CustomDataLoader('Data')
-    num_samples = len(my_dataloader)
-    samples = random.sample(np.arange(num_samples), train_size+test_size)
+    num_samples = len(my_dataset)
+    samples = random.sample(list(np.arange(num_samples)), train_size+test_size)
 
-    train_data = np.zeros(train_size)
+    train_data = []
     train_samples = samples[:train_size]
-    for i, s in enumerate(train_samples):
-        train_data[i] = my_dataloader[s]
-    train_dataloader = torch.utils.data.DataLoader(train_data, batch_size=batch_size)
+    for s in train_samples:
+        train_data.append(my_dataset[s])
+    train_dataloader = DataLoader(train_data, batch_size=batch_size)
 
+    test_data = []
     test_samples = samples[train_size:]
-    test_data = np.zeros(test_size)
-    for i, s in enumerate(test_samples):
-        test_data[i] = my_dataloader[s]
-    test_dataloader = torch.utils.data.DataLoader(test_data, batch_size=batch_size)
+    for s in test_samples:
+        test_data.append(my_dataset[s])
+    test_dataloader = DataLoader(test_data, batch_size=batch_size)
     
     loss_fn = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
