5f216b88e7f701a7f9056533ce641f41895d6039
Michael Shea Jacobs <M.S.Jacobs2727@gmail.com>
2022-04-29 16:20:52 -0400

    updated train_network.py

--- Molecular-Fingerprint-Code/train_network.py
+++ Molecular-Fingerprint-Code/train_network.py
@@ -8,20 +8,45 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 
 class NeuralNet(torch.nn.Module):
-    def __init__(self, *num_per_layer):
+    """The convolutional neural network.
+    TODO: Make hyperparameters accessible and tune.
+    """
+    def __init__(self):
+        """Input:
+                np.array of size 32x32 of type np.float32
+                Two convolutional layers, three fully connected layers. 
+                Shape of data progresses as follows:
+
+                Input:          (32, 32)
+                Unflatten:      ( 1, 32, 32)
+                Conv2d:         ( 6, 30, 30)
+                Pool:           ( 6, 15, 15)
+                Conv2d:         (16, 13, 13)
+                Pool:           (16,  6,  6)
+                Flatten:        (576,) [ = 16*6*6]
+                FCL:            (64,)
+                FCL:            (64,)
+                FCL:            (3,)
+        """
         super(NeuralNet, self).__init__()
-        self.flatten = torch.nn.Flatten()
-        seq = []
-        for l1, l2 in zip(num_per_layer[:-1], num_per_layer[1:]):
-            seq.append(torch.nn.Linear(l1, l2))
-            seq.append(torch.nn.ReLU())
-        seq.append(torch.nn.Linear(num_per_layer[-1], 3))
-        self.linear_relu_stack = torch.nn.Sequential(*seq)
-    
-    def forward(self, x):
-        x = self.flatten(x)
-        logits = self.linear_relu_stack(x)
-        return logits
+
+        self.forward = torch.nn.Sequential(
+            # Convolutional layers
+            torch.nn.Unflatten(1, (1, 32)),
+            torch.nn.Conv2d(1, 6, 3), 
+            torch.nn.ReLU(), 
+            torch.nn.MaxPool2d(2),
+            torch.nn.Conv2d(6, 16, 3), 
+            torch.nn.ReLU(), 
+            torch.nn.MaxPool2d(2),
+            # Fully connected layers
+            torch.nn.Flatten(),
+            torch.nn.Linear(16*6*6, 64), 
+            torch.nn.ReLU(),
+            torch.nn.Linear(64, 64), 
+            torch.nn.ReLU(),
+            torch.nn.Linear(64, 3)
+        )
 
 class CustomDataset(Dataset):
     """Pixelated viscosity plots dataset. Each datum is a 32x32 grid of values
@@ -69,11 +94,10 @@ def train(dataloader, model, loss_fn, optimizer, device):
         optimizer.step()
 
         if batch_num % 4 == 0:
-            loss, current = loss.item(), (batch_num + 1) * len(X)
-            print(f'{loss:.7f} [{current:.5d}/{size:.5d}]')
+            loss, current = loss.item(), (batch_num + 4) * len(X)
+            print(f'{loss = :>7f} [{current:>5d}/{size:>5d}]')
 
 def test(dataloader, model, loss_fn, device):
-    size = len(dataloader.dataset)
     num_batches = len(dataloader)
     model.eval()
     avg_loss, avg_error = 0, 0
@@ -87,51 +111,53 @@ def test(dataloader, model, loss_fn, device):
     avg_loss /= num_batches
     avg_error /= num_batches
     avg_error = torch.mean(avg_error, 0)
-    print(f'{avg_error[0]:.3f}, {avg_error[1]:.3f}, {avg_error[2]:.3f}, {avg_loss:.3f}')
+    print(f'{avg_error[0]:>3f}, {avg_error[1]:>3f}, {avg_error[2]:>3f}, {avg_loss = :>3f}')
 
 def main():
+    """Load dataset, then train and test network over several epochs.
+    TODO: Simplify data generation, record only Bg, Bth, Pe values, then
+    generate data on the fly, perhaps with GPU.
+    """
     random.seed(5)
-    batch_size = 64
-    train_size = 8192
-    test_size = 256
+    batch_size = 1000
+    train_size = 50000
+    test_size = 10000
     
-    print('Loading dataset...')
     my_dataset = CustomDataset('../grid_data')
-    print('Done.')
-    first_layer_shape = my_dataset[0][0].shape
-    first_layer_len = first_layer_shape[0] * first_layer_shape[1]
-    num_per_layer = [first_layer_len, 64, 64]
+    print('Loaded dataset.')
 
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
-    model = NeuralNet(*num_per_layer).to(device)
+    model = NeuralNet().to(device)
     print('Loaded model.')
 
     num_samples = len(my_dataset)
-    samples = random.sample(list(np.arange(num_samples)), train_size+test_size)
+    for i in range(5):
+        print(f'\n****Epoch #{i}****\n')
+        samples = random.sample(list(np.arange(num_samples)), train_size+test_size)
 
-    train_data = []
-    train_samples = samples[:train_size]
-    for s in train_samples:
-        train_data.append(my_dataset[s])
-    train_dataloader = DataLoader(train_data, batch_size=batch_size)
-    print('Set training data.')
+        train_data = []
+        train_samples = samples[:train_size]
+        for s in train_samples:
+            train_data.append(my_dataset[s])
+        train_dataloader = DataLoader(train_data, batch_size=batch_size)
+        print(f'Set training data, length {len(train_dataloader):d}.')
 
-    test_data = []
-    test_samples = samples[train_size:]
-    for s in test_samples:
-        test_data.append(my_dataset[s])
-    test_dataloader = DataLoader(test_data, batch_size=batch_size)
-    print('Set testing data.')
+        test_data = []
+        test_samples = samples[train_size:]
+        for s in test_samples:
+            test_data.append(my_dataset[s])
+        test_dataloader = DataLoader(test_data, batch_size=batch_size)
+        print(f'Set testing data, length {len(test_dataloader)}.')
 
-    loss_fn = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+        loss_fn = torch.nn.CrossEntropyLoss()
+        optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
 
-    print('Training...')
-    train(train_dataloader, model, loss_fn, optimizer, device)
-    print('Done.\nTesting...')
-    test(test_dataloader, model, loss_fn, device)
-    print('Done.')
+        print('Training...')
+        train(train_dataloader, model, loss_fn, optimizer, device)
+        print('Done.\nTesting...')
+        test(test_dataloader, model, loss_fn, device)
+        print('Done.')
 
 if __name__ == '__main__':
     main()
