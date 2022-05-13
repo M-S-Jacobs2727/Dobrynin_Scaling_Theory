1a7210b1f6e1d3c0dac4f2efb6848fe308d2a57c
M-S-Jacobs2727 <42037940+M-S-Jacobs2727@users.noreply.github.com>
2022-05-13 14:26:12 -0400

    Linting for Mike

--- Mike_network_test/gen_and_train.py
+++ Mike_network_test/gen_and_train.py
@@ -2,15 +2,16 @@ import torch
 
 import scaling_torch_lib as scaling
 
-class NeuralNet(torch.nn.Module):   
+
+class NeuralNet(torch.nn.Module):
     """The classic, fully connected neural network.
     TODO: Make hyperparameters accessible and tune.
     """
     def __init__(self, shape):
         """Input:
                 np.array of size 32x32 of type np.float32
-        
-        Three fully connected layers. 
+
+        Three fully connected layers.
         Shape of data progresses as follows:
 
                 Input:          (32, 32)
@@ -24,24 +25,25 @@ class NeuralNet(torch.nn.Module):
         self.conv_stack = torch.nn.Sequential(
             # Fully connected layers
             torch.nn.Flatten(),
-            torch.nn.Linear(shape[0]*shape[1], 128), 
+            torch.nn.Linear(shape[0]*shape[1], 128),
             torch.nn.ReLU(),
-            torch.nn.Linear(128, 64), 
+            torch.nn.Linear(128, 64),
             torch.nn.ReLU(),
             torch.nn.Linear(64, 3)
         )
-    
+
     def forward(self, x):
         return self.conv_stack(x)
 
-class ConvNeuralNet(torch.nn.Module):   
+
+class ConvNeuralNet(torch.nn.Module):
     """The convolutional neural network.
     TODO: Make hyperparameters accessible and tune.
     """
     def __init__(self, shape):
         """Input:
                 np.array of size 32x32 of type np.float32
-                Two convolutional layers, three fully connected layers. 
+                Two convolutional layers, three fully connected layers.
                 Shape of data progresses as follows:
 
                 Input:          (32, 32)
@@ -60,35 +62,32 @@ class ConvNeuralNet(torch.nn.Module):
         self.conv_stack = torch.nn.Sequential(
             # Convolutional layers
             torch.nn.Unflatten(1, (1, 32)),
-            torch.nn.Conv2d(1, 6, 3), 
-            torch.nn.ReLU(), 
+            torch.nn.Conv2d(1, 6, 3),
+            torch.nn.ReLU(),
             torch.nn.MaxPool2d(2),
-            torch.nn.Conv2d(6, 16, 3), 
-            torch.nn.ReLU(), 
+            torch.nn.Conv2d(6, 16, 3),
+            torch.nn.ReLU(),
             torch.nn.MaxPool2d(2),
             # Fully connected layers
             torch.nn.Flatten(),
-            torch.nn.Linear(16*6*6, 64), 
+            torch.nn.Linear(16*6*6, 64),
             torch.nn.ReLU(),
-            torch.nn.Linear(64, 64), 
+            torch.nn.Linear(64, 64),
             torch.nn.ReLU(),
             torch.nn.Linear(64, 3)
         )
-    
+
     def forward(self, x):
         return self.conv_stack(x)
 
+
 def train(model, loss_fn, optimizer, device,
-        num_samples, batch_size, resolution):
+          num_samples, batch_size, resolution):
     model.train()
     num_batches = num_samples // batch_size
-    print_every = 10
+    # print_every = 10
     for b, (X, y) in enumerate(scaling.surface_generator(
-            num_batches, 
-            batch_size, 
-            device,
-            resolution=resolution
-        )):
+            num_batches, batch_size, device, resolution=resolution)):
         pred = model(X)
         loss = loss_fn(pred, y)
 
@@ -100,19 +99,21 @@ def train(model, loss_fn, optimizer, device,
         #     loss, current = loss.item(), (b + 1) * batch_size
         #     print(f'[{current:>7d}/{num_samples:>7d}]')
         #     mean_error = torch.mean(torch.abs(y - pred) / y, 0)
-        #     print(f'\tmean_error = {mean_error[0]:.3f} {mean_error[1]:.3f} {mean_error[2]:.3f}')
+        #     print(f'\tmean_error = {mean_error[0]:.3f} {mean_error[1]:.3f}'
+        #           ' {mean_error[2]:.3f}')
         #     print(f'\t{loss = :>7f}')
 
+
 def test(model, loss_fn, device,
-        num_samples, batch_size, resolution):
+         num_samples, batch_size, resolution):
     model.eval()
     avg_loss = 0
     avg_error = 0
     num_batches = num_samples // batch_size
     with torch.no_grad():
         for b, (X, y) in enumerate(scaling.surface_generator(
-            num_batches, 
-            batch_size, 
+            num_batches,
+            batch_size,
             device,
             resolution=resolution
         )):
@@ -121,48 +122,51 @@ def test(model, loss_fn, device,
 
             avg_loss += loss.item()
             avg_error += torch.mean(torch.abs(y - pred) / y, 0)
-    
+
     avg_loss /= num_batches
     avg_error /= num_batches
 
     print(f'Accuracy:\n\t{avg_loss = :>5f}\n\taverage errors ='
-        f' {avg_error[0]:>5f} {avg_error[1]:>5f} {avg_error[2]:>5f}'
-    )
+          f' {avg_error[0]:>5f} {avg_error[1]:>5f} {avg_error[2]:>5f}'
+          )
+
 
 def main():
     batch_size = 1000
     train_size = 500000
     test_size = 100000
-    
-    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+    device = torch.device('cuda') if torch.cuda.is_available() else \
+        torch.device('cpu')
     print(f'{device = }')
 
     loss_fn = torch.nn.MSELoss()
 
-    for i, resolution in enumerate([(32, 32), (64, 64), (96, 96)]):        
+    for i, resolution in enumerate([(32, 32), (64, 64), (96, 96)]):
         print(f'\n*** Resolution {resolution} ***')
 
         model = NeuralNet(resolution).to(device)
         print('Loaded model.')
-    
+
         for j in range(10):
             print(f'* Epoch {j} *')
-            
+
             optimizer = torch.optim.SGD(
-                        model.parameters(), 
-                        lr=0.1, 
+                        model.parameters(),
+                        lr=0.1,
                         momentum=0.9/(j+1)
             )
 
             print('Training')
             train(model, loss_fn, optimizer, device,
-                train_size, batch_size, resolution
-            )
+                  train_size, batch_size, resolution
+                  )
 
             print('Testing')
             test(model, loss_fn, device,
-                test_size, batch_size, resolution
-            )
+                 test_size, batch_size, resolution
+                 )
+
 
 if __name__ == '__main__':
     main()
--- Mike_network_test/scaling_torch_lib.py
+++ Mike_network_test/scaling_torch_lib.py
@@ -1,5 +1,4 @@
 import collections
-import itertools as it
 import numpy as np
 import torch
 
@@ -13,6 +12,7 @@ BG = Param(0.3, 1.6)
 BTH = Param(0.2, 0.9)
 PE = Param(2, 20)
 
+
 def unnormalize_params(y):
     """Simple linear normalization.
     """
@@ -21,11 +21,12 @@ def unnormalize_params(y):
     Pe = y[:, 2] * (PE.max - PE.min) + PE.min
     return Bg, Bth, Pe
 
-def normalize_visc(eta_sp : torch.Tensor):
+
+def normalize_visc(eta_sp: torch.Tensor):
     """Add noise, cap the values, take the log, then normalize.
     """
     eta_sp += eta_sp * 0.05 * torch.normal(
-        torch.zeros_like(eta_sp), 
+        torch.zeros_like(eta_sp),
         torch.ones_like(eta_sp)
     )
     eta_sp = torch.fmin(eta_sp, ETA_SP.max)
@@ -33,15 +34,17 @@ def normalize_visc(eta_sp : torch.Tensor):
     return (torch.log(eta_sp) - torch.log(ETA_SP.min)) / \
         (torch.log(ETA_SP.max) - torch.log(ETA_SP.min))
 
-def surface_generator(num_batches : int, batch_size : int, 
-        device : torch.device, resolution=(32, 32)):
-    """Generate `batch_size` surfaces, based on ranges for `Bg`, `Bth`, and 
-    `Pe`, to be used in a `for` loop. 
 
-    It defines the resolution of the surface based on either user input 
-    (keyword argument `resolution`). It then generates random values for `Bg`, 
+def surface_generator(num_batches: int, batch_size: int,
+                      device: torch.device,
+                      resolution: 'tuple[int]' = (32, 32)):
+    """Generate `batch_size` surfaces, based on ranges for `Bg`, `Bth`, and
+    `Pe`, to be used in a `for` loop.
+
+    It defines the resolution of the surface based on either user input
+    (keyword argument `resolution`). It then generates random values for `Bg`,
     `Bth`, and `Pe`, evaluates the `(phi, Nw, eta_sp)` surface, and normalizes
-    the result. The normalized values of `eta_sp` and `(Bg, Bth, Pe)` are 
+    the result. The normalized values of `eta_sp` and `(Bg, Bth, Pe)` are
     yielded as `X` and `y` for use in a neural network.
 
     Input:
@@ -52,17 +55,17 @@ def surface_generator(num_batches : int, batch_size : int,
             of the generated values.
 
     Output:
-        `X` (`torch.Tensor` of size `(batch_size, *resolution)`) : Generated, 
+        `X` (`torch.Tensor` of size `(batch_size, *resolution)`) : Generated,
             normalized values of `eta_sp` at indexed `phi` and `Nw`.
         `y` (`torch.Tensor` of size `(batch_size, 3)`) : Generated, normalized
             values of `(Bg, Bth, Pe)`.
     """
-    
+
     ETA_SP.min.to(dtype=torch.float, device=device)
     ETA_SP.max.to(dtype=torch.float, device=device)
-    
+
     # Create tensors for phi (concentration) and Nw (chain length)
-    # Both are meshed and tiled to cover a 3D tensor of size 
+    # Both are meshed and tiled to cover a 3D tensor of size
     # (batch_size, *resolution) for simple, element-wise operations
     phi = torch.tensor(np.geomspace(
         PHI.min,
@@ -99,7 +102,7 @@ def surface_generator(num_batches : int, batch_size : int,
         )
 
         # Number of repeat units per entanglement strand
-        # Universal definition of Ne accounts for both 
+        # Universal definition of Ne accounts for both
         # Kavassalis-Noolandi and Rubinstein-Colby scaling
         Ne = Pe**2 * g * torch.fmin(
             torch.tensor([1], device=device), torch.fmin(
@@ -125,8 +128,10 @@ def surface_generator(num_batches : int, batch_size : int,
         X = normalize_visc(eta_sp).to(torch.float)
         yield X, y
 
-def voxel_image_generator(num_batches : int, batch_size : int, 
-        device : torch.device, resolution : tuple = (32, 32, 32)):
+
+def voxel_image_generator(num_batches: int, batch_size: int,
+                          device: torch.device,
+                          resolution: 'tuple[int]' = (32, 32, 32)):
     """Uses surface_generator to generate a surface with a resolution one more,
     then generates a 3D binary array dictating whether or not the surface
     passes through a given voxel. This is determined using the facts that:
@@ -151,26 +156,30 @@ def voxel_image_generator(num_batches : int, batch_size : int,
             values of `(Bg, Bth, Pe)`.
     """
     s_res = (resolution[0] + 1, resolution[1] + 1)
-    
+
     eta_sp = normalize_visc(torch.tensor(
-        np.geomspace(ETA_SP.min, ETA_SP.max, resolution[2]+1, endpoint=True), 
-        dtype=torch.float, 
+        np.geomspace(ETA_SP.min, ETA_SP.max, resolution[2]+1, endpoint=True),
+        dtype=torch.float,
         device=device
     ))
 
-    for X, y in surface_generator(num_batches, batch_size, device, resolution=s_res):
+    for X, y in surface_generator(num_batches, batch_size, device,
+                                  resolution=s_res):
         # # Full loop-tard. Never go full loop-tard. For verification only.
         # image = torch.zeros((batch_size, *resolution))
-        # for b, i, j, k in it.product(range(batch_size), range(resolution[0]), 
+        # for b, i, j, k in it.product(range(batch_size), range(resolution[0]),
         #         range(resolution[1]), range(resolution[2])):
         #     image[b, i, j, k] = 1 if (
-        #         eta_sp[k+1] > X[b, i, j] 
+        #         eta_sp[k+1] > X[b, i, j]
         #         and eta_sp[k] < X[b, i+1, j+1]
         #     ) else 0
         # print(torch.sum(torch.logical_and(X<1, X>0)))
         # print(torch.sum(image))
 
-        surf = torch.tile(X.reshape((batch_size, *s_res, 1)), (1, 1, 1, resolution[2]+1))
+        surf = torch.tile(
+            X.reshape((batch_size, *s_res, 1)),
+            (1, 1, 1, resolution[2]+1)
+        )
         image = torch.logical_and(
             surf[:, :-1, :-1, :-1] < eta_sp[1:],
             surf[:, 1:, 1:, 1:] > eta_sp[:-1]
@@ -178,14 +187,16 @@ def voxel_image_generator(num_batches : int, batch_size : int,
             dtype=torch.float,
             device=device
         )
-        
+
         yield image, y
+
 
 def main():
     """For testing only.
     """
     for surf in voxel_image_generator(8, 1, torch.device('cpu')):
         X, y = surf
+
 
 if __name__ == '__main__':
     main()
